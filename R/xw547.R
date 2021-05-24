196163a0f212e33fa5c03decb15e5a999a4d58fd
Xiaohan Wang <xw547@cornell.edu>
2021-05-24 00:38:58 -0400

    revise on vignette

---
+++ R/xw547.R
@@ -0,0 +1,12 @@
+#' xw547: A package that integrates hw1 from BTRY6520
+#'
+#' This package gives a comprehensive integration of the existing functions of
+#' the first homework.
+#'
+#' @section Functions:
+#' solve_ols, algo_leverage, elnet_coord
+#'
+#' @docType package
+#' @name overview
+NULL
+#> NULL
